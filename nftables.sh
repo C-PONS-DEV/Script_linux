148cad295f757c3dfc1806a5965cbd09848e22a1
Clément PONS <clementpons@MacBook-Pro-de-Clement.local>
2021-09-27 10:36:45 +0200

    V1

---
+++ nftables.sh
@@ -0,0 +1,29 @@
+# Script sauvegarde, et démarage automatique des regles Nftabes
+
+# PONS Clément SIO SISR 2
+
+# Sauvegarde : 
+
+echo 'Quel et le nom de votre table ?'
+read matable
+
+nft list table $matable > nftables.rules
+
+# Application des règles au démarage :
+
+rm /etc/network/interfaces
+
+echo Quelle ip ?
+read ip
+echo Quelle Gateway ?
+read gateway
+
+echo source /etc/network/interfaces.d/* >> /etc/network/interfaces
+echo auto lo >> /etc/network/interfaces
+echo iface lo inet loopback >> /etc/network/interfaces
+echo allow-hotplug enp0s3 >> /etc/network/interfaces
+echo auto enp0s3 >> /etc/network/interfaces
+echo iface enp0s3 inet static >> /etc/network/interfaces
+echo address $ip >> /etc/network/interfaces
+echo gateway $gateway >> /etc/network/interfaces
+echo pre-up nft –f /root/nftables/iptables.rules
