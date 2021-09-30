5faccbb39c1444ba816043d7752fc98499641b8d
CPONS <cponsdev@gmail.com>
2021-09-30 17:08:20 +0200

    modification cote

--- sauvegarde.sh
+++ sauvegarde.sh
@@ -1,3 +1,3 @@
 cp -r /media/docpartgae /tmp
-tar -czvf /tmp/'date +%Y-%m-%d'_pons_sauvegarde.tar.gz /media/docpartgae
+tar -czvf /tmp/`date +%Y-%m-%d`_pons_sauvegarde.tar.gz /media/docpartgae
 rm -r /tmp/docpartgae
