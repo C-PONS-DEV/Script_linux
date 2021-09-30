f41f2bed415cc0d5d5d16466b50d1a9fb0da956f
CPONS <cponsdev@gmail.com>
2021-09-30 15:40:27 +0200

    modification

--- sauvegarde.sh
+++ sauvegarde.sh
@@ -1,29 +1,5 @@
-mysqldump --user=shovon --password=123 --all-databases > /tmp/sauvegarde.sql
+tar -czvf `date +%Y-%m-%d`_pons_sauvegarde.tar.gz /partage
 
-echo "" >> /tmp/sauvegarde.log
+cp `date +%Y-%m-%d`_pons_sauvegarde.tar.gz /tmp
 
-echo "-------------" >> /tmp/sauvegarde.log
-
-echo "" >> /tmp/sauvegarde.log
-
-echo "`date +%Y-%m-%d__%Hh%M` : Début de la sauvegarde" >> /tmp/sauvegarde.log
-
-echo "`date +%Y-%m-%d__%Hh%M` : Début de la sauvegarde"
-
-tar -czvf Archive_`date +%Y-%m-%d`.tar.gz /tmp/sauvegarde.sql
-
-cp Archive_`date +%Y-%m-%d`.tar.gz /tmp
-
-rm Archive_`date +%Y-%m-%d`.tar.gz
-
-echo "`date +%Y-%m-%d__%Hh%M` : Sauvegarde terminee... Supression de sauvegarde.sql" >> /tmp/sauvegarde.log
-
-echo "`date +%Y-%m-%d__%Hh%M` : Sauvegarde terminee... Supression de sauvegarde.sql"
-
-echo "" >> /tmp/sauvegarde.log
-
-echo "-------------" >> /tmp/sauvegarde.log
-
-read -p "Appuyer sur une touche pour continuer..."
-
-rm /tmp/sauvegarde.sql
+rm `date +%Y-%m-%d`_pons_sauvegarde.tar.gz
