e56020d18ee68c4c061ed19900806179160a4ca1
CPONS <cponsdev@gmail.com>
2021-09-30 16:57:49 +0200

    ajout

---
+++ sauvegarde-config.sh
@@ -0,0 +1,13 @@
+echo "Quel et le dossier a sauvegarder ? (media/partage)"
+read dosssauv
+
+echo "Quel et le dossier de destination ?(tmp)"
+read dossdest
+
+echo "Quel et le nom de la sauvegarde ?"
+read name
+
+
+
+cp -r /$dosssauv /$dossdest
+tar -czvf /$dossdest'date +%Y-%m-%d'$name.tar.gz /$dosssauv
